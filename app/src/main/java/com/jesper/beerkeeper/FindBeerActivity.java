6fb73a23a9e900c28000ed31ab7050ee34c770f9
Eugene <jespert@mail.ru>
2018-06-30 19:35:11 +0300

    Migrated to butterknife

--- app/src/main/java/com/jesper/beerkeeper/FindBeerActivity.java
+++ app/src/main/java/com/jesper/beerkeeper/FindBeerActivity.java
@@ -9,21 +9,30 @@ import android.widget.TextView;
 import java.util.List;
 import org.w3c.dom.Text;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class FindBeerActivity extends Activity {
+
+    @BindView(R.id.brands)
+    TextView brands;
+
+    @BindView(R.id.color)
+    Spinner color;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+        ButterKnife.bind(this);
     }
 
     private BeerExpert expert = new BeerExpert();
 
-    public void onClickFindBeer (View view){
-        //Получение ссылки на TextView
-        TextView brands = (TextView) findViewById(R.id.brands);
-        //Получиение ссылки на Spinner
-        Spinner color = (Spinner) findViewById(R.id.color);
+    @OnClick (R.id.button)
+    void onClickFindBeer (){
         //Получение варианта, выбранный в Spinner
         String beerType = String.valueOf(color.getSelectedItem());
         List<String> brandList = expert.getBrands(beerType);
